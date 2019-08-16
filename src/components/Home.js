b8df04ecd3b1a84dc55d5b890c63c178e2350a38
sheik oodally <sheikoodally@hotmail.com>
2019-08-15 21:42:57 -0400

    adding items in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import phone1 from '../shoppingimg/phone/phone1.jpg'
+import phone2 from '../shoppingimg/phone/phone2.jpg'
+import phone3 from '../shoppingimg/phone/phone3.jpg'
 import './css/Home.css';
 
 export class Home extends Component {
 
-    render() {
-        
+    render() {     
         return (
 
             <div className="home">
@@ -15,10 +17,14 @@ export class Home extends Component {
                     return <div key = {phones.id} className="colorPattern">
                             <h3 id="brand"> {phones.brand}</h3>
                             <h4 id="model"> {phones.model}</h4>
+                            <table>
+                            <tr><p className="Phoneprice">Price: {phones.price}$</p></tr>
+                            <tr><p>Camera: {phones.camera}</p></tr> 
+                            <tr><p>Battery capacity: {phones.battery}</p></tr>
+                            </table>
                             </div>        
                     })}
                 </section>
-
             </div>
 
         )
